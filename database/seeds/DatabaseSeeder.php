27fc9121713a8dd245bb529f8b78fd4c0ec78846
Leo <leandro.mdrs06@gmail.com>
2019-05-09 23:25:47 -0300

    Adicionado Seed's

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('coordenadores')->insert([
+            'name' => 'Coordenador',
+            'email' => 'coordenador@gmail.com',
+            'password' => Hash::make('12345678')
+        ]);
+        // DB::table('professors')->insert([
+        //     'name' => 'Professor',
+        //     'email' => 'professor@gmail.com',
+        //     'password' => Hash::make('12345678')
+        // ]);
+        // DB::table('alunos')->insert([
+        //     'name' => 'Aluno',
+        //     'email' => 'aluno@gmail.com',
+        //     'password' => Hash::make('12345678')
+        // ]);
     }
 }
