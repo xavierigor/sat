4f85f6d2c63e8b6d18a5c363ff49d724e8b73e39
Leo <leandro.mdrs06@gmail.com>
2019-05-26 13:00:18 -0300

    Ajuste de responsividade na sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,19 +1,63 @@
 $(document).ready(function() {
-    if($(window).width() > 767 && !$('#mySidenav').hasClass('open')) {
-        $('#mySidenav').addClass('open');
-    } else if ($(window).width() <= 767 && $('#mySidenav').hasClass('open')) {
-        $('#mySidenav').removeClass('open');
-    }
+    
+    // Execultar ao iniciar
+    toglleIfScreen();
+    
+    // Execultar se tela redimensionar
+    $(window).resize(function() {
+        toglleIfScreen();
+    });
+
 })
 
-function toggleSidebar() {
-    if($("#mySidenav").hasClass('open')) {
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-        $("#mySidenav").toggleClass('open');
-    } else {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        $("#mySidenav").toggleClass('open');
+function toglleIfScreen(){
+
+    // if($(window).width() > 767 && !$('#mySidenav').hasClass('open')) {
+    //     $('#mySidenav').addClass('open');
+    //     toggleSidebar();
+    // } else if ($(window).width() < 767 && $('#mySidenav').hasClass('open')) {
+    //     $('#mySidenav').removeClass('open');
+    //     toggleSidebar();
+    // }
+
+    if($(window).width() > 767 ){
+        $('#mySidenav').addClass('closed');
+        toggleSidebar();
+    } else if ($(window).width() < 767){
+        $('#mySidenav').removeClass('closed');
+        toggleSidebar();
     }
 }
+
+function toggleSidebar() {
+    if($("#mySidenav").hasClass('closed')) {
+        document.getElementById("mySidenav").style.width = "250px";
+        pushPageContent();
+        $("#mySidenav").toggleClass('closed');
+    } else {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        $("#mySidenav").toggleClass('closed');
+    }
+}
+
+// Empurrar conteudo do main apenas se a tela for grande. Por questões de responsividade
+function pushPageContent(){
+    
+    if($(window).width() > 767 ){
+        document.getElementById("main").style.marginLeft = "250px";
+    }else{
+        document.getElementById("main").style.marginLeft = "0";
+    }
+}
+
+
+// Escurecer conteudo da página, caso sidebar expandida em tela pequena
+// function darkenPageContent(){
+    
+//     if($("#main").hasClass('overlaid')){
+//         $('#main').removeClass('overlaid');
+//     } else {
+//         $('#main').addClass('overlaid');
+//     }
+// }
